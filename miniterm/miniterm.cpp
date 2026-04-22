303bb7d9e9134f3f9e3223d3f6e1c4a7f28b5d40
Knightmare <knightmare2600@users.noreply.github.com>
2026-04-22 16:29:18 +0100

    Update miniterm.cpp

--- miniterm/miniterm.cpp
+++ miniterm/miniterm.cpp
@@ -1,6 +1,6 @@
 /**
  * =============================================================================
- * miniterm.c - ConPTY WinPE Terminal (Faxe Kondi Edition)
+ * miniterm.cpp - ConPTY WinPE Terminal (Faxe Kondi Edition)
  * =============================================================================
  *
  *  Project:     miniterm
@@ -32,8 +32,8 @@
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <https://www.gnu.org/licenses/>.
  * ============================================================================
- */
-
+ *
+ *
  *
  * =============================================================================
  * VERSION HISTORY
